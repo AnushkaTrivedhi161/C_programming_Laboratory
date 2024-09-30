9024ff9d0d41d319261e71cfdcbc24065fb7ebeb
AnushkaTrivedhi161 <anushkatrivedhi23@gmail.com>
2024-09-30 21:26:49 +0530

    Create  Area_Perimeter_rect.c

---
+++ Area_Perimeter_rect.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int l,b,A,P;
+    scanf("%d%d",&l,&b);
+    A=(l*b);
+    P=2*(l+b);
+    printf("The Area is %d\n The Perimeter is %d",A,P);
+    return 0;
+}
