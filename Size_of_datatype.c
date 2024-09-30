9b52b579896d82a44b64008bc65f1ec184ee1a20
AnushkaTrivedhi161 <anushkatrivedhi23@gmail.com>
2024-09-30 21:33:48 +0530

    Create Size_of_datatype.c

---
+++ Size_of_datatype.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  printf("The Size of int:%u",sizeof(int));
+  printf("The Size of float:%u",sizeof(float));
+  printf("The Size of char:%u",sizeof(char));
+  printf("The Size of double:%u",sizeof(double));
+  return 0;
+}
