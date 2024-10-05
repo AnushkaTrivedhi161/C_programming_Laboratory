f4091b50fee0e9ec62b9d84be31206829ef7356a
AnushkaTrivedhi161 <anushkatrivedhi23@gmail.com>
2024-10-05 21:21:10 +0530

    Create Equal_or_not.c

---
+++ Equal_or_not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() 
+{   
+   int a,b;
+   scanf("%d%d",&a,&b); 
+   if(a==b)
+   {
+     printf("The numbers are equal");
+   } 
+   else 
+   { 
+     printf("The numbers are not equal");
+   } 
+   return 0; 
+}
