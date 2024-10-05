fad6837ebfe6267e6a7593ed9d9d4e7cce49c298
AnushkaTrivedhi161 <anushkatrivedhi23@gmail.com>
2024-10-05 21:16:45 +0530

    Create Largest_2_int_no.c

---
+++ Largest_2_int_no.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+   int a,b; 
+   scanf("%d%d",&a,&b); 
+   if(a>b) 
+   { 
+      printf("%d is the largest number than %d",a,b);
+   } 
+   else if(b>a) 
+   { 
+      printf("%d is the largest number than %d",b,a); 
+   } 
+   else 
+   {
+      printf("They are equal"); 
+   } 
+   return 0;
+}
